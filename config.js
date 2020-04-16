cd927fbbf5eab5dd9e1bdd53f1c0aab213a8a4dc
Sasha Brown <sashadbrown@gmail.com>
2020-04-16 15:03:00 +0100

    upload

---
+++ config.js
@@ -0,0 +1,195 @@
+var config = {
+    style: 'mapbox://styles/mapbox/streets-v11',
+    accessToken: 'pk.eyJ1Ijoic2FzaGFzYXlzaGkiLCJhIjoiY2s5MDc5MWYwMDJlajNsbnRnOXIwazloYyJ9.m4VBn5t5ZCIJT4Mmj9L-Xw',
+    showMarkers: false,
+    theme: 'light',
+    alignment: 'right',
+    title: '',
+    subtitle: '',
+    byline: 'Countermapping the Irish Carceral State',
+    footer: 'Source: Countermapping the Irish Carceral State -- Vukašin, Joe and Sasha',
+    chapters: [
+        {
+            id: 'direct-provision',
+            title: 'Direct Provision in Ireland',
+            image: '',
+            description: 'The Direct Provision System in Ireland began as a temporary measure in 1999 as public provision of accomodation for asylum seekers. Twenty years later, the Direct Provision system operates as de facto detention for asylum seekers and as emergency homeless accomodation for hundreds of families and individuals who have residency status in Ireland. <br> Direct Provision is operated by the Reception and Integration Agency (RIA), a sub-agency of the Department of Justice and Equality. The RIA contracts much of the work to private businesses. According to annual reports, RIA operated 92 Direct Provisions centres from 2004 to 2017. The Asylum Archive documents over 150 current and former Direct Provision centres in Ireland. <br> The map documents these centres.',
+            location: {
+              center: [-6.73254, 54.09889],
+              zoom: 5.99,
+              pitch: 0.00,
+              bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'vukas-story',
+            title: 'Vukašin’s Story',
+            image: '',
+            description: '',
+            location: {
+              center: [-8.24632, 53.81813],
+              zoom: 8.08,
+              pitch: 0.00,
+              bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'part-one',
+            title: 'Part One',
+            image: 'images/Beech lawn Direct Provision Centre, Wicklow.jpg',
+            description: '<blockquote>I look through my stained window<br>There are fields in the distance<br>They seem too far away.<br>I can’t see the greenness of the fields.</blockquote> <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/688400569%3Fsecret_token%3Ds-2x1jc&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>',
+            location: {
+              center: [-8.24632, 53.81813],
+              zoom: 8.08,
+              pitch: 0.00,
+              bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+id: 'part-two',
+title: 'Part Two',
+image: 'images/Birchwood House Direct Provision Centre, Waterford.jpg',
+description: '<blockquote>My window is divided in half.<br>There are yellow marks on both sides of the window.<br>The mark on the left side is bigger and wider than the mark on the right side of the window</blockquote><iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/688400566%3Fsecret_token%3Ds-YVbzn&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>',
+
+location: {
+              center: [-8.764414, 53.760837],
+              zoom: 13,
+              pitch: 0.00,
+              bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+
+
+
+},
+{
+id: 'part-three',
+title: 'Part Three',
+image: 'images/Globe House Direct Provision Centre, Sligo.jpg',
+description: '<blockquote>The next morning a woman followed by two security officers arrived in Kilmacud house direct provision centre.</blockquote><iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/688400560%3Fsecret_token%3Ds-gVFCj&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>',
+
+location: {
+              center: [-8.764414, 53.760837],
+              zoom: 13,
+              pitch: 0.00,
+              bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+
+
+},
+{
+id: 'part-four',
+title: 'Part Four',
+image: 'images/Lissywollen Accommodation Direct Provision Centre, Athlone (1).jpg',
+description: '<blockquote>There is a christmas tree in the centre<br>The christmas tree is in the recreation room</blockquote><iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/688400554%3Fsecret_token%3Ds-Jpvro&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>',
+
+location: {
+              center: [-8.764414, 53.760837],
+              zoom: 17,
+              pitch: 0.00,
+              bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+
+
+},
+{
+id: 'part-five',
+title: 'Part Five',
+image: 'images/Mount Trenchard Direct Provision Centre, Foynes (3).jpg',
+description: '<blockquote>At the reception in the centre I notice a monitor.</blockquote><iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/688400545%3Fsecret_token%3Ds-fSyTc&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>',
+
+location: {
+              center: [-8.764414, 53.760837],
+              zoom: 17,
+              pitch: 0.00,
+              bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+
+
+},
+{
+id: 'part-six',
+title: 'Part Six',
+image:'images/Southern Hills Direct Provision Centre, Galway.jpg',
+description: '<blockquote>Outside the main building of the centre there is a wooden bench.</blockquote><iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/688400539%3Fsecret_token%3Ds-BaN1E&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>',
+
+location: {
+              center: [-8.764414, 53.760837],
+              zoom: 17,
+              pitch: 0.00,
+              bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+
+
+
+},
+{
+id: 'part-seven',
+title: 'Part Seven',
+image: 'images/Railway Hostel Direct Provision Centre, Kiltimagh (3).jpg',
+description: '<blockquote>It is warm in my room.<br>The floor squeaks under my shoes.<br>I look at the ceiling.</blockquote><iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/688400536%3Fsecret_token%3Ds-1QS9f&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>',
+
+location: {
+              center: [-8.764414, 53.760837],
+              zoom: 17,
+              pitch: 0.00,
+              bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+
+
+
+},
+{
+id: 'part-eight',
+title: 'Part Eight',
+image: 'images/The Old Convent Direct Provision Centre, Ballyhaunis (8).jpg',
+description: '<blockquote>I go to the canteen to make instant coffee.<br>You can&rsquo;t be drinking too many coffees per day,<br>The coffee is expensive.</blockquote><iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/688400533%3Fsecret_token%3Ds-HaLcn&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>',
+
+location: {
+              center: [-8.764414, 53.760837],
+              zoom: 17,
+              pitch: 0.00,
+              bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+
+
+
+},
+{
+id: 'part-nine',
+title: 'Part Nine',
+image:'images/The Old School Direct Provision Centre,Dunlaoghaire.jpg',
+description: '<blockquote>In the night I wake up.<br>I gently open the door of my room.<br>I close the door.<br>The key is in my pocket.</blockquote><iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/688400527%3Fsecret_token%3Ds-BtxnZ&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>',
+
+location: {
+              center: [-8.764414, 53.760837],
+              zoom: 17,
+              pitch: 0.00,
+              bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+
+
+
+}
+]};
