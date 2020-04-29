44ec0d7ac0c1f21f2fc23f30882d4c67f8027441
Sasha Brown <sashadbrown@gmail.com>
2020-04-30 00:00:43 +0100

    big update of three maps

--- config.js
+++ config.js
@@ -9,20 +9,7 @@ var config = {
     byline: '',
     footer: 'Source: Countermapping the Irish Carceral State -- Vukašin, Joe and Sasha',
     chapters: [
-        {
-            id: 'direct-provision',
-            title: 'Direct Provision in Ireland',
-            image: '',
-            description: 'The Direct Provision System in Ireland began as a temporary measure in 1999 as public provision of accomodation for asylum seekers. Twenty years later, the Direct Provision system operates as de facto detention for asylum seekers and as emergency homeless accomodation for hundreds of families and individuals who have residency status in Ireland. <br> Direct Provision is operated by the Reception and Integration Agency (RIA), a sub-agency of the Department of Justice and Equality. The RIA contracts much of the work to private businesses. According to annual reports, RIA operated 92 Direct Provisions centres from 2004 to 2017. The Asylum Archive documents over 150 current and former Direct Provision centres in Ireland. <br> The map documents these centres.',
-            location: {
-              center: [-6.73254, 54.09889],
-              zoom: 5.99,
-              pitch: 0.00,
-              bearing: 0.00
-            },
-            onChapterEnter: [],
-            onChapterExit: []
-        },
+
         {
             id: 'vukas-story',
             title: 'Vukašin’s Story',
---
+++ config_intro.js
@@ -0,0 +1,26 @@
+var config = {
+    style: 'mapbox://styles/countermapping-ireland/ck24pi8wy0qfb1cpgej4mc2xc',
+    accessToken: 'pk.eyJ1IjoiY291bnRlcm1hcHBpbmctaXJlbGFuZCIsImEiOiJjazI0b3p4Y2MyNWJoM2htdmZiNDgxdWxnIn0.95fvRhIeqfnjE5ioOdSAkw',
+    showMarkers: false,
+    theme: 'light',
+    alignment: 'right',
+    title: '',
+    subtitle: 'Countermapping the Irish Carceral State',
+    byline: '',
+    footer: 'Source: Countermapping the Irish Carceral State -- Vukašin, Joe, Japhet and Sasha',
+    chapters: [
+        {
+            id: 'direct-provision',
+            title: 'Direct Provision in Ireland',
+            image: '',
+            description: 'The Direct Provision System in Ireland began as a temporary measure in 1999 as public provision of accomodation for asylum seekers. Twenty years later, the Direct Provision system operates as de facto detention for asylum seekers and as emergency homeless accomodation for hundreds of families and individuals who have residency status in Ireland. <br> Direct Provision is operated by the Reception and Integration Agency (RIA), a sub-agency of the Department of Justice and Equality. The RIA contracts much of the work to private businesses. According to annual reports, RIA operated 92 Direct Provisions centres from 2004 to 2017. The Asylum Archive documents over 150 current and former Direct Provision centres in Ireland. <br><br> The map documents these centres.<br><br><a href = "vukas_story.html">Vukasins Story</a><br><a href = "services access map.html">Direct Provision: access to services</a>',
+            location: {
+              center: [-6.73254, 54.09889],
+              zoom: 5.99,
+              pitch: 0.00,
+              bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+]};
